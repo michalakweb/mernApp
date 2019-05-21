2e14253064d70e336086de7dcc91276281cd9970
Mateusz Michalak <michalakweb@gmail.com>
2019-05-21 10:01:03 +0200

    created schema

---
+++ models/Item.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+const ItemSchema = new Schema({
+    customerID: {
+        type: Number,
+        required: true
+    },
+    name: {
+        first: {
+            type: String,
+            required: true
+        },
+        last: {
+            type: String,
+            required: true
+        }
+    },
+    birthday: {
+        type: Date
+    },
+    gender: {
+        type: String
+    },
+    lastContact: {
+        type: Date
+    },
+    customerLifetimeValue: {
+        type: Number
+    }
+});
