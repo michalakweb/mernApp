fdeca2c877df5eb955653a57ea05fdd6b0cbfc4a
Mateusz Michalak <michalakweb@gmail.com>
2019-05-21 11:33:35 +0200

    app displays data from the backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,34 @@
 import React from 'react';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Hello
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    data: []
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  getData() {
+    fetch('/api/items')
+    .then(response => response.json())
+    .then((data) => {
+      data = Object.values(data);
+      this.setState({data: data});
+    })
+  }
+
+  render() {
+    return (
+      <div>
+          Hello
+          {
+          this.state.data.map((el: any) => <p key={el._id}>{el.name.first}</p>)
+          }
+      </div>
+    )
+  }
 }
 
 export default App;
