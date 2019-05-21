c562e562b7a9b710057766fef413ab4364efb991
Mateusz Michalak <michalakweb@gmail.com>
2019-05-21 10:53:12 +0200

    delete route works

--- routes/api/items.js
+++ routes/api/items.js
@@ -13,7 +13,7 @@ router.get('/', (req, res) => {
 });
 
 // @route POST api/items
-// @description GET All Items
+// @description POST Items
 // @access Public
 router.post('/', (req, res) => {
     const newItem = new Item({
@@ -30,6 +30,15 @@ router.post('/', (req, res) => {
 
     newItem.save()
     .then(item => res.json())
+});
+
+// @route DELETE api/items
+// @description DELETE an Item
+// @access Public
+router.delete('/:id', (req, res) => {
+    Item.findById(req.params.id)
+    .then(item => item.remove().then(() => res.json({success: true})))
+    .catch(err => res.status(404).json({success: false}))
 });
 
 
